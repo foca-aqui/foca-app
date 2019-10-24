f6a1f3ec23756ca2060f9029171899055617cf68
will <william.g.neto@gmail.com>
2019-10-24 14:07:26 -0300

    parlamentares

--- frontend/src/main/parlamentares.jsx
+++ frontend/src/main/parlamentares.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import "./parlamentares.css"
 
 export default props => {
     if (props.data) {
         return (
             <div className="parlamentares row">
                 <div className="col-sm-6">
+                    <b>Deputados Federais</b>
                     { Object.entries(props.data.federais).map( ([key, obj]) =>(
-                        <span>{key} </span>
+                        <span className="dep-fed">{key} </span>
                     ))}
                 </div>
                 <div className="col-sm-6">
-                { Object.entries(props.data.estaduais).map( ([key, obj]) =>(
-                        <span>{key} </span>
+                    <b>Deputados Estaduais</b>
+                    { Object.entries(props.data.estaduais).map( ([key, obj]) =>(
+                        <span className="dep-est">{key} </span>
                     ))}
                 </div>
                 
